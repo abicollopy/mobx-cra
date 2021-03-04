bc725c3026fa530009aba6f9e97c12dba40d0a96
abicollopy <a.collopy@hotmail.co.uk>
2021-03-04 15:12:54 +0000

    mobx ticker

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
 import './App.css';
+import { makeAutoObservable } from "mobx";
+import { observer } from "mobx-react";
 
-function App() {
+class Incrementer {
+  number = 0
+
+  constructor() {
+      makeAutoObservable(this)
+  };
+
+  increase() {
+      this.number += 1
+  };
+
+  reset() {
+      this.number = 0
+  };
+};
+
+const Ticker = observer(({ myIncrementer }: { myIncrementer:Incrementer }) => {
   return (
-    <div className="App">
+    <div className="body">
+      <div className="buttonContainer">
+        <div
+          className="button"
+          onClick={() => {
+            myIncrementer.increase();
+          }}>Click Me!</div>
+          <div
+          className="button"
+          onClick={() => {
+            myIncrementer.reset();
+          }}>Reset</div>
+      </div>
+      <div className="display">The number is {myIncrementer.number}</div>
     </div>
   );
-}
+});
+
+const App = () => {
+  const myIncrementer = new Incrementer();
+  return(
+    <Ticker myIncrementer={myIncrementer} />
+  );
+};
 
 export default App;
